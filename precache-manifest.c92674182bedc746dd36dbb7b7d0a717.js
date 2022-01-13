6e00db52805b34f78f661d8ebcd58c869357608b
BlueYang <2931442305@qq.com>
2022-01-13 12:22:07 +0800

    deploy

---
+++ precache-manifest.c92674182bedc746dd36dbb7b7d0a717.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a3a700c6221cd18a8e",
+    "url": "/BlueMao-bookkeeping-website/css/app.89c22063.css"
+  },
+  {
+    "revision": "0f40f5be311e51bebebc",
+    "url": "/BlueMao-bookkeeping-website/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/BlueMao-bookkeeping-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/BlueMao-bookkeeping-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "69d0cc1f393233b3375a3ade70fc0bf3",
+    "url": "/BlueMao-bookkeeping-website/img/background1.69d0cc1f.png"
+  },
+  {
+    "revision": "029ddd56bb69fdc2ef8fd79fcf564e23",
+    "url": "/BlueMao-bookkeeping-website/img/background2.029ddd56.png"
+  },
+  {
+    "revision": "713a4368b03fa80aafe11834a7e37752",
+    "url": "/BlueMao-bookkeeping-website/index.html"
+  },
+  {
+    "revision": "83a3a700c6221cd18a8e",
+    "url": "/BlueMao-bookkeeping-website/js/app.ff480d43.js"
+  },
+  {
+    "revision": "0f40f5be311e51bebebc",
+    "url": "/BlueMao-bookkeeping-website/js/chunk-vendors.a78e32df.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/BlueMao-bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BlueMao-bookkeeping-website/robots.txt"
+  }
+]);
